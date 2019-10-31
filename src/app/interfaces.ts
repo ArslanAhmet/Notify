4739900c8ff3f44aee98ac95ff669a283204f8d4
Ahmet Arslan <arslanahmet1981@gmail.com>
2019-10-31 23:00:29 +0200

    güncelleme

---
+++ src/app/interfaces.ts
@@ -0,0 +1,30 @@
+export interface EventResponse {
+    event: EmergencyEvent;
+    links: Links;
+}
+export interface EmergencyEvent {
+id: number;
+created: Date;
+caller: Caller;
+dialed: string;
+}
+
+export interface Caller {
+    id: string;
+    firstName: string;
+    lastName: string;
+    phone: string;
+    adress: Address;
+}
+export interface Address {
+    street1: string;
+    street2: string;
+    city: string;
+    state: string;
+    zip: string;
+}
+export interface Links {
+    self: string;
+    next: string;
+    acknowledgements: string;
+}
